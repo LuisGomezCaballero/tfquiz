c78a05b5105b41f04153c16b7640bce48d8e7e7e
luisgomezcaballero <luis.gomez.caballero@gmail.com>
2018-01-27 11:06:16 +0100

    Confiurate Spring Boot + Spring MVC + Thymeleaf

---
+++ tfquiz/src/main/java/com/luisgomezcaballero/tfquiz/TfquizApplication.java
@@ -0,0 +1,12 @@
+package com.luisgomezcaballero.tfquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfquizApplication.class, args);
+	}
+}
